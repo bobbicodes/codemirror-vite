5eb7b4315434e7fa1eb80d439dbff68fe344eb63
Bobby Towers <porkostomus@mail.com>
2023-06-21 22:51:27 -0700

    credits

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # lang-clojure-eval
 
-A language provider based on the [Lezer Clojure](https://github.com/nextjournal/lezer-clojure) parser, with inline-evaluation support via the [Small Clojure Interpreter (SCI)](https://github.com/babashka/sci).
+A Codemirror 6 language extension based on the [Lezer Clojure](https://github.com/nextjournal/lezer-clojure) parser, with inline-evaluation support via the [Small Clojure Interpreter (SCI)](https://github.com/babashka/sci).
 
 ## Demo
 
@@ -11,4 +11,8 @@ npm run dev
 
 ## Credits
 
-The Nextjournal team's efforts are what made this possible, it would have taken me years to figure all of this out. The [lang-clojure](https://github.com/nextjournal/lang-clojure/) project includes the Lezer parser extended with highlighting and indentation information, and [clojure-mode](https://github.com/nextjournal/clojure-mode/tree/main) is a Clojurescript library that showed me everything I needed to know to learn Codemirror 6. Finally, Michiel Borkent for [SCI](https://github.com/babashka/sci) which powers in-browser evaluation, which is compiled into an ESM module with [shadow-cljs](https://github.com/thheller/shadow-cljs).
+- The [lang-clojure](https://github.com/nextjournal/lang-clojure/) project includes the Lezer parser extended with highlighting and indentation information.
+
+- Evaluation functionality ported from Nextjurnal's [clojure-mode](https://github.com/nextjournal/clojure-mode/tree/main).
+
+- [SCI](https://github.com/babashka/sci) compiled to ESM with [shadow-cljs](https://github.com/thheller/shadow-cljs).
--- eval-region.js
+++ eval-region.js
@@ -1,7 +1,6 @@
 import { Prec } from '@codemirror/state'
 import { keymap } from '@codemirror/view'
 import { syntaxTree } from "@codemirror/language"
-
 import { evalString } from "./sci"
 
 function up(node) {
@@ -15,48 +14,52 @@ function topType(nodeType) {
 function evalCell(view) {
     console.log(evalString(view.state.doc.text.join(" ")))
     return true
-  }
-  
-  function mainSelection(state) {
-    return state.selection.asSingle().ranges[0]
-  }
-  
-  function tree(state, pos, dir) {
-    switch (arguments["length"]) {
-      case 1:
-        return syntaxTree(state);
-      case 2:
-        return syntaxTree(state).resolveInner(pos);
-      case 3:
-        return syntaxTree(state).resolveInner(pos, dir);
-    }
-  }
+}
 
-  function nearestTouching(state, pos, dir) {
+function mainSelection(state) {
+    return state.selection.asSingle().ranges[0]
+}
+
+function tree(state, pos, dir) {
+    switch (arguments["length"]) {
+        case 1:
+            return syntaxTree(state);
+        case 2:
+            return syntaxTree(state).resolveInner(pos);
+        case 3:
+            return syntaxTree(state).resolveInner(pos, dir);
+    }
+}
+
+function nearestTouching(state, pos, dir) {
     const L = tree(state, pos, -1)
     const R = tree(state, pos, 1)
     const mid = tree(state, pos)
-  
+
     return mid
-  }
-  
-  function nodeAtCursor(state, from) {
+}
+
+function nodeAtCursor(state, from) {
     return nearestTouching(state, from, -1)
-  }
-  
-  function cursorNodeString(state) {
+}
+
+function cursorNodeString(state) {
     return nodeAtCursor(state)
-  }
-  
-  function evalAtCursor(view) {
-    console.log(nearestTouching(view.state, ))
+}
+
+function evalAtCursor(view) {
+    console.log(nearestTouching(view.state,))
     return true
-  }
+}
 
 export function evalExtension() {
     return Prec.highest(keymap.of(
-      [{key: "Shift-Enter",
-        run: evalCell},
-        {key: "Ctrl-Enter",
-        run: evalAtCursor}]))
-  }
+        [{
+            key: "Shift-Enter",
+            run: evalCell
+        },
+        {
+            key: "Ctrl-Enter",
+            run: evalAtCursor
+        }]))
+}
