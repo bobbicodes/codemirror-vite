f03e4247d8d808c66613b1c6d827537845adb9fa
Bobby Towers <porkostomus@mail.com>
2023-06-24 18:26:08 -0700

    detect platform in keymap display

--- main.js
+++ main.js
@@ -61,12 +61,35 @@ let editorState = EditorState.create({
   extensions: [basicSetup, clojure(), evalExtension()]
 })
 
+function isLinux() {
+  if (navigator.userAgent.match(/(Linux)|(X11)/g) === null) {
+      return false
+  }
+  return true
+}
+
+function isMac() {
+  if (!isLinux &&
+      navigator.userAgent.match(/(Mac)|(iPhone)|(iPad)|(iPod)/g) != null) {
+      return true
+  }
+  return false
+}
+
+function modifier() {
+  if (isMac()) {
+      return "Cmd"
+  } else {
+      return "Alt"
+  }
+}
+
 new EditorView({
   state: editorState,
   parent: document.querySelector('#app')
 }).focus()
 
-let topLevelText = "Alt/Cmd+Enter = Eval top-level form"
+let topLevelText = modifier() + "+Enter = Eval top-level form"
 let keyBindings = "<strong>Key bindings:</strong>,Shift+Enter = Eval cell," + 
                    topLevelText + ",Ctrl+Enter = Eval at cursor";
 keyBindings = keyBindings.split(',');
