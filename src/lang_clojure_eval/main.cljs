d87917044dc87d8ee19a91eab376a00ff9224fd3
Bobby Towers <porkostomus@mail.com>
2023-06-24 21:37:47 -0700

    pass sci/init empty options map

--- src/lang_clojure_eval/main.cljs
+++ src/lang_clojure_eval/main.cljs
@@ -1,8 +1,8 @@
 (ns lang-clojure-eval.main
   (:require [sci.core :as sci]))
 
-(defn sci-init [opts]
-  (sci/init opts))
+(defn sci-init []
+  (sci/init {}))
 
 (defn eval-string [ctx s]
   (str (sci/eval-string* ctx s)))
